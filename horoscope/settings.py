a30539df4797cf7d1cd813abacdaf8e4724a4529
Phung Tien Minh <phungtienminh@gmail.com>
2023-08-05 16:39:04 +0700

    Add external host

--- horoscope/settings.py
+++ horoscope/settings.py
@@ -31,6 +31,10 @@ DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
